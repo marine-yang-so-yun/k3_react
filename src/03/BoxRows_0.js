5adcd1e640ae4e060670568d3ae9e18db8e8ba5b
marine-yang-so-yun <marina0406@naver.com>
2023-04-25 17:22:50 +0900

    20230425

---
+++ src/03/BoxRows_0.js
@@ -0,0 +1,48 @@
+
+
+const BoxRows = (probs) => {
+
+    const mvlist = [...probs.mv];
+    console.log("boxrows", mvlist);
+
+
+
+
+    let trTags = [];
+    for (let row of mvlist) {
+        console.log(row.rank, row.movieNm, row.salesAmt, row.rankInten);
+
+        let icon;
+        var rank = parseInt(row.rankInten);
+        if (rank == 0)  {
+            icon = '⏺'
+        }
+        else if (rank > 0)  {
+            icon = '🔼'
+        }
+        else    {
+            icon = '🔽'
+        }
+
+        trTags.push(
+            <tr className = "mytr" >
+            <td>{row.rank}</td>
+            <td>{row.movieNm}</td>
+            <td>{parseInt(row.salesAmt).toLocaleString()}</td>
+            <td>{icon}{Math.abs(rank)}</td>
+           
+
+        </tr>
+        );
+
+    }
+
+    console.log(trTags);
+    return (
+        <>
+            {trTags}
+        </>
+    );
+}
+
+export default BoxRows;
